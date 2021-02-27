4a2d46d500d2ccca3868ce16afad26e8c6b1143c
eric gilchrist <eric.gilchrist.ic@smartsheet.com>
2021-02-27 04:42:02 -0800

    Added/fixed sort functionality with multiple categories. Started work on pagination   - page loads 5 offers initially   - show more button shows the next 5 with each click Subtle animation added to the button   - on click it mimmicks the appearance of being pressed Added event listeners for the select input and the button click

--- offers.js
+++ offers.js
@@ -1,40 +1,102 @@
-fetch("https://convoy-mock-api.herokuapp.com/offers")
-    .then(res => res.json())
-    .then((data) => {
-        let offers = data.map(genTemplate).join("\n");
-        document.getElementById("offerList").innerHTML=offers;
-}).catch(err => console.error(err));
+let currentPage = 1;
+const pageListSize = 5;
+const startIndex = () => { return currentPage * pageListSize - pageListSize };
+const endIndex = () => { return currentPage * pageListSize };
+const numberOfPages = () => {
+    return Math.ceil(data.length / pageListSize);
+};
+let offersList = [];
+let pageList = [];
+let renderPage = [];
 
-const genTemplate = (item) => {
+const getOffers = (prop1, prop2, prop3) => {
+    document.getElementById("offerList").innerHTML="";
+    currentPage = 1;
+    fetch("https://convoy-mock-api.herokuapp.com/offers")
+        .then(res => res.json())
+        .then((data) => {
+            offersList = data.sort(sortJSON(prop1, prop2, prop3));
+            pageList = () => { return offersList.slice(startIndex(), endIndex()) };
+            renderPage = pageList().map(genTemplate).join("\n");
+            document.getElementById("offerList").innerHTML=renderPage;
+    }).catch(err => console.error(err));
+}
+
+const sortJSON = (prop1, prop2, prop3) => {
+    return function(a, b) {
+        let textA = !prop3
+            ? !prop2
+                ? a[prop1]
+                : a[prop1][prop2]
+            : a[prop1][prop2][prop3];
+        let textB = !prop3
+            ? !prop2
+                ? b[prop1]
+                : b[prop1][prop2]
+            : b[prop1][prop2][prop3];
+        console.log("prop1", prop1, "prop2", prop2, "prop3", prop3, textA, textB);
+        return (textA < textB)
+            ? -1
+            : (textA > textB)
+            ? 1
+            : 0;
+    }
+}
+
+const genTemplate = item => {
     const local = new Date();
     const timezoneOffset = local.getTimezoneOffset() / 60;
     const pickupDateStart = item.origin.pickup.start.split("T")[0];
-    const pickupDateEnd = item.origin.pickup.end.split("T")[0] === pickupDateStart ? "" : item.origin.pickup.end.split("T")[0] + ", ";
+    const pickupDateEnd = item.origin.pickup.end.split("T")[0] === pickupDateStart
+        ? ""
+        : item.origin.pickup.end.split("T")[0] + ", ";
     let pickupTimeStart = item.origin.pickup.start.split("T")[1].substring(0,5);
-    let ptsHour = parseInt(pickupTimeStart.substring(0,2)) - timezoneOffset < 0 ? parseInt(pickupTimeStart.substring(0,2)) - timezoneOffset + 24 : parseInt(pickupTimeStart.substring(0,2)) - timezoneOffset;
+    let ptsHour = parseInt(pickupTimeStart.substring(0,2)) - timezoneOffset < 0 
+        ? parseInt(pickupTimeStart.substring(0,2)) - timezoneOffset + 24
+        : parseInt(pickupTimeStart.substring(0,2)) - timezoneOffset;
     if(ptsHour < 10) {
         ptsHour = "0" + ptsHour;
     }
     pickupTimeStart = ptsHour + pickupTimeStart.substring(2, 6);
-    let pickupTimeEnd = item.origin.pickup.end.split("T")[1].substring(0,5) === pickupTimeStart ? "" : item.origin.pickup.end.split("T")[1].substring(0,5);
-    let pteHour = pickupTimeEnd.length > 0 && parseInt(pickupTimeEnd.substring(0,2)) - timezoneOffset < 0 ? parseInt(pickupTimeEnd.substring(0,2)) - timezoneOffset + 24 : parseInt(pickupTimeEnd.substring(0,2)) - timezoneOffset;
+    let pickupTimeEnd = item.origin.pickup.end.split("T")[1].substring(0,5) === pickupTimeStart
+        ? ""
+        : item.origin.pickup.end.split("T")[1].substring(0,5);
+    let pteHour = pickupTimeEnd.length > 0 && parseInt(pickupTimeEnd.substring(0,2)) - timezoneOffset < 0
+        ? parseInt(pickupTimeEnd.substring(0,2)) - timezoneOffset + 24
+        : parseInt(pickupTimeEnd.substring(0,2)) - timezoneOffset;
     if (pteHour < 10) {
         pteHour = "0" + pteHour;
     }
     pickupTimeEnd = pteHour + pickupTimeEnd.substring(2, 6);
     const startPickup = pickupDateStart + ", " + pickupTimeStart;
-    const endPickup = !pickupDateEnd ? pickupTimeEnd : pickupDateEnd + pickupTimeEnd;
-    const pickupWindow_isRange = !pickupDateEnd && !pickupTimeEnd ? false : true;
-    const pickupWindow = pickupWindow_isRange ? startPickup + " - " + endPickup : startPickup;
-    const connectorLength = pickupWindow_isRange ? "longer" : "";
+    const endPickup = !pickupDateEnd 
+        ? pickupTimeEnd
+        : pickupDateEnd + pickupTimeEnd;
+    const pickupWindow_isRange = !pickupDateEnd && !pickupTimeEnd
+        ? false
+        : true;
+    const pickupWindow = pickupWindow_isRange
+        ? startPickup + " - " + endPickup
+        : startPickup;
     const dropoffDateStart = item.destination.dropoff.start.split("T")[0];
-    const dropoffDateEnd = item.destination.dropoff.end.split("T")[0] === dropoffDateStart ? "" : item.destination.dropoff.end.split("T")[0] + ", ";
+    const dropoffDateEnd = item.destination.dropoff.end.split("T")[0] === dropoffDateStart
+        ? ""
+        : item.destination.dropoff.end.split("T")[0] + ", ";
     const dropoffTimeStart = item.destination.dropoff.start.split("T")[1].substring(0,5);
-    const dropoffTimeEnd = item.destination.dropoff.end.split("T")[1].substring(0,5) === dropoffTimeStart ? "" : item.destination.dropoff.end.split("T")[1].substring(0,5);
+    const dropoffTimeEnd = item.destination.dropoff.end.split("T")[1].substring(0,5) === dropoffTimeStart
+        ? ""
+        : item.destination.dropoff.end.split("T")[1].substring(0,5);
     const startDropoff = dropoffDateStart + ", " + dropoffTimeStart;
-    const endDropoff = !dropoffDateEnd ? dropoffTimeEnd : dropoffDateEnd + dropoffTimeEnd;
-    const dropoffWindow_isRange = !dropoffDateEnd && !dropoffTimeEnd ? false : true;
-    const dropoffWindow = dropoffWindow_isRange ? startDropoff + " - " + endDropoff : startDropoff;
+    const endDropoff = !dropoffDateEnd
+        ? dropoffTimeEnd
+        : dropoffDateEnd + dropoffTimeEnd;
+    const dropoffWindow_isRange = !dropoffDateEnd && !dropoffTimeEnd
+        ? false
+        : true;
+    const dropoffWindow = dropoffWindow_isRange
+        ? startDropoff + " - " + endDropoff
+        : startDropoff;
+    const price = Math.trunc(item.offer);
 
     return `<div class="offer">
                 <div class="offer-label">
@@ -44,7 +106,7 @@ const genTemplate = (item) => {
                     <div class="offer-details-waypoints">
                         <div class="offer-details-waypoints-connectorDots">
                             <div class="offer-details-waypoints-connectorDots-dot"></div>
-                            <div class="offer-details-waypoints-connectorDots-connector ${connectorLength}"></div>
+                            <div class="offer-details-waypoints-connectorDots-connector"></div>
                             <div class="offer-details-waypoints-connectorDots-dot"></div>
                         </div>
                         <div class="offer-details-waypoints-terminalss">
@@ -59,9 +121,41 @@ const genTemplate = (item) => {
                         </div>
                     </div>
                     <div class="offer-details-mileage">${item.miles} miles</div>
-                    <div class="offer-details-price">$${item.offer}</div>
+                    <div class="offer-details-price">$${price}</div>
                 </div>
             </div>`
+};
+
+(init => {
+    getOffers("origin", "pickup", "start");
+})();
+
+document.addEventListener("input", (e) => {
+    if (e.target.id !== "selectSortCriteria") {
+        return;
     }
+    const option = {
+        pickup: "origin,pickup,start",
+        dropoff: "destination,dropoff,start",
+        price: "offer",
+        origin: "origin,city",
+        destination: "destination,city",
+        distance: "miles"
+    }    
+    let args = option[e.target.value].split(',');    
+    console.log(args[0] + ', ' + args[1] + ', ' + args[2]);
+    args.length === 3
+        ? getOffers(args[0], args[1], args[2])
+        : args.length === 2
+        ? getOffers(args[0], args[1])
+        : getOffers(args[0]);
+});
 
-
+document.addEventListener("click", (e) => {
+    if (e.target.id === "nextPage") {
+        document.getElementById("offerList").innerHTML="";
+        currentPage += 1;
+        renderPage = pageList().map(genTemplate).join("\n");
+        document.getElementById("offerList").innerHTML=renderPage;
+    }
+});
